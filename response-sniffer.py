9a48aa8cc171dd3bcf2334d30552e5006dd64c54
0x00A7AFBC <64595170+0x00A7AFBC@users.noreply.github.com>
2024-09-22 03:59:05 +0200

    Delete response-sniffer.py

--- response-sniffer.py
+++
@@ -1,8 +0,0 @@
-from mitmproxy import http, ctx
-
-class ResponseSniffer:
-    def response(self, flow: http.HTTPFlow) -> None:
-        print(flow.response)
-        print(flow.response.text)
-
-addons = [ResponseSniffer()]
